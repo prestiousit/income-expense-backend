4ffa9ff6f07fd537b19df1e425b4f2fe6cd0f654
savaniarpita39 <savaniarpita39@gmail.com>
2024-01-23 12:15:23 +0530

    transction api add

--- api/index.js
+++ api/index.js
@@ -6,4 +6,5 @@ const apiRouter = express.Router();
 apiRouter.use("/userAdmin", require("./routes/userAdmin"));
 apiRouter.use("/bank", require("./routes/bank"));
 apiRouter.use("/user", require("./routes/user"));
+apiRouter.use("/transction", require("./routes/transction"));
 module.exports = apiRouter;
