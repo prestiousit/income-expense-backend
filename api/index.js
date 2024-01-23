54af9d8b0bb0232ea51f61be78b34e5d9ebc424c
milanahir227 <milanahir227@gmail.com>
2024-01-23 12:06:25 +0530

    user api create

--- api/index.js
+++ api/index.js
@@ -4,5 +4,6 @@ const db = require("../config/database");
 const apiRouter = express.Router();
 
 apiRouter.use("/userAdmin", require("./routes/userAdmin"));
-apiRouter.use("/bank", require("./routes/bank/index.js"));
+apiRouter.use("/bank", require("./routes/bank"));
+apiRouter.use("/user", require("./routes/user"));
 module.exports = apiRouter;
