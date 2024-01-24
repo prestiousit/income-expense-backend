7ea9d3662c06e1af2c987edfb600c94e2cbac2ae
savaniarpita39 <savaniarpita39@gmail.com>
2024-01-24 18:55:24 +0530

    label api created

--- api/index.js
+++ api/index.js
@@ -7,4 +7,5 @@ apiRouter.use("/userAdmin", require("./routes/userAdmin"));
 apiRouter.use("/bank", require("./routes/bank"));
 apiRouter.use("/user", require("./routes/user"));
 apiRouter.use("/transaction", require("./routes/transaction"));
+apiRouter.use("/label", require("./routes/label"));
 module.exports = apiRouter;
