4f7f2c452c5ab3f7ddd12b78a96c5bf616d0786c
mrabhidudhat <mrabhidudhat@gmail.com>
2024-01-24 16:29:36 +0530

    transaction apis query update

--- api/controllers/user/index.js
+++ api/controllers/user/index.js
@@ -1,31 +1,24 @@
-const moment = require("moment");
 const db = require("../../../config/database");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const UserCreate = async (req, res) => {
+const AdminUserRegister = async (req, res) => {
   try {
-    const { name, description, mobileNo, status } = req.body;
+    const { username, password } = req.body;
 
-    if (!name) throw new Error("Name is Required..!");
-    if (!status) req.body.status = "active";
-
-    //  req.body.createdBy = "" //pass admin id using auth
-    req.body.isDeleted = 0;
-    req.body.createdAt = moment().format("YYYY-MM-DD hh:mm:ss");
-
-    const field = Object.keys(req.body)
-      .map((key) => key)
-      .toString();
-    const value = Object.keys(req.body)
-      .map((key) => `'${req.body[key]}'`)
-      .toString();
-
-    const Query = `INSERT INTO user (${field}) VALUES (${value})`;
-
-    const [data] = await db.promise().query(Query);
-    res.status(200).json({
+    if (!username || !password) {
+      throw new Error("Username or Password is Required");
+    } 
+    req.body.password = await bcrypt.hash(password, 10);
+    const [user] = await db
+      .promise()
+      .query("INSERT INTO adminUser (username,password) VALUES (?,?)", [
+        username,
+        req.body.password,
+      ]);
+    res.status(201).json({
       status: "success",
-      message: "user Created successfully",
-      data,
+      message: "user created successfully",
     });
   } catch (error) {
     res.status(404).json({
@@ -34,21 +27,99 @@ const UserCreate = async (req, res) => {
     });
   }
 };
-const UserGet = async (req, res) => {
-  try {
-    const Query = "select id,name from user";
-    const [user] = await db.promise().query(Query);
 
-    const data = await user.map((el) => {
-      return {
-        value: el.id,
-        label: el.name,
-      };
-    });
+const AdminUserLogin = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const [user] = await db
+      .promise()
+      .query("SELECT * FROM adminUser WHERE username = ?", [username]);
+
+    if (!user || user.length === 0) {
+      throw new Error("Incorrect username or password");
+    }
+
+    const checkpaasword = await bcrypt.compare(
+      password,
+      user[0].password
+    );
+    if (!checkpaasword) {
+      throw new Error("Incorrect username or 'password'");
+    }
+
+    const token = jwt.sign({ id: user[0].id,username : user[0].username }, process.env.SECRET_KEY);
+
+
     res.status(200).json({
       status: "success",
-      message: "user find successfully",
-      data,
+      message: "Login successfully",
+      user: user[0],
+      token
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
+
+const AdminUserDelete = async (req, res) => {
+  try {
+    const userId = req.query.id;
+
+    const [user] = await db
+      .promise()
+      .query("SELECT * FROM adminUser WHERE id = ?", [userId]);
+
+    if (!user || user.length === 0) {
+      throw new Error("user not found");
+    }
+
+    const [deleteuser] = await db
+      .promise()
+      .query("DELETE FROM adminUser WHERE id = ?", [userId]);
+
+    res.status(200).json({
+      status: "success",
+      message: "deleted successfully",
+      user: deleteuser,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
+
+const AdminUserUpdate = async (req, res) => {
+  try {
+    const userId = req.query.id;
+    const [user] = await db
+      .promise()
+      .query("SELECT * FROM adminUser WHERE id = ?", [userId]);
+
+    if (!user || user.length === 0) {
+      throw new Error("user not found");
+    }
+    let { username, password } = req.body;
+
+    username = username ?? user[0].username;
+    password = password ?? user[0].password;
+
+    const [updateuser] = await db
+      .promise()
+      .query(
+        "UPDATE adminUser SET username = ? , password = ? WHERE id = ?",
+        [username, password, userId]
+      );
+
+    res.status(200).json({
+      status: "success",
+      message: "updated successfully",
+      user: updateuser,
     });
   } catch (error) {
     res.status(404).json({
@@ -59,6 +130,8 @@ const UserGet = async (req, res) => {
 };
 
 module.exports = {
-  UserCreate,
-  UserGet,
+  AdminUserRegister,
+  AdminUserLogin,
+  AdminUserDelete,
+  AdminUserUpdate
 };
